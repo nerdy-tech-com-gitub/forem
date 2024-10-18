0c58b78a041d39e3567e43cfbf3d537fbba034fa
Collette (Jitera) <developers@jitera.com>
2024-10-18 03:56:35 +0000

    feat[database]: create table 'longdong8inches' Changes * '/app/models/longdong8inches.rb': created * '/db/migrate/20231018102332_create_longdong8inches.rb': created

---
+++ app/models/longdong8inches.rb
@@ -0,0 +1,2 @@
+class Longdong8inches < ApplicationRecord
+end
---
+++ db/migrate/20231018102332_create_longdong8inches.rb
@@ -0,0 +1,7 @@
+class CreateLongdong8inches < ActiveRecord::Migration[6.1]
+  def change
+    create_table :longdong8inches do |t|
+      t.timestamps
+    end
+  end
+end
